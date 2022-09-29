427778edf2a154b9afe1c0901fc4922c27f1203d
Shirshak Roy <75179984+Shirshakhtml@users.noreply.github.com>
2022-09-29 23:14:00 +0530

    Create weather.sh

---
+++ weather.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright 2022 SH14SH4K. All rights reserved.
+
+
+case $1 in
+-h | --help)
+	echo "$program $version"
+	echo "Copyright $year $developer. All rights reserved."
+	echo
+	echo "Usage: weather [options]"
+	echo "Option          Long Option             Description"
+	echo "-h              --help                  Show the help screen"
+	echo "-l [location]   --location [location]   Specifies the location"
+	;;
+-l | --location)
+	curl https://wttr.in/$2
+	;;
+*)
+	curl https://wttr.in
+	;;
+esac
